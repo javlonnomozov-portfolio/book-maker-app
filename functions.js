ff023a1998461bd529fe039d9fffa1506e46f9bf
Javlon Nomozov (Portfolios) <javlonnomozov.portfolio@gmail.com>
2024-09-29 14:37:34 +0300

    final of 8 sheets on per page

--- functions.js
+++ functions.js
@@ -10,6 +10,27 @@ function bookMaker4Pages(pageCount) {
   return [r1, r2];
 }
 
+function bookMaker8Pages(pageCount) {
+  const r1 = [];
+  const r2 = [];
+  for (let i = 0; i <= pageCount / 2 - 4; i += 4) {
+    r1.push((i / 4 + 1) * 2);
+    r1.push(pageCount - (i / 4 + 1) * 2 + 1);
+    r1.push((i / 4 + 1) * 2 + pageCount / 4);
+    r1.push(pageCount - ((i / 4 + 1) * 2 + pageCount / 4) + 1);
+
+    r2.push(pageCount - (i / 4 + 1) * 2 + 2);
+    r2.push((i / 4 + 1) * 2 - 1);
+    r2.push(pageCount - ((i / 4 + 1) * 2 + pageCount / 4) + 2);
+    r2.push((i / 4 + 1) * 2 + pageCount / 4 - 1);
+  }
+  return [r1, r2];
+}
+
+// console.log(bookMaker8Pages(16));
+
+bookMaker8Pages(16);
+
 function bookMaker4Pages2Sides(pageCount) {
   const result = [];
   for (let i = 0; i <= pageCount / 2 - 2; i += 2) {
